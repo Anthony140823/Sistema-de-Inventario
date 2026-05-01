7435a548554eb0683d3226153997dbc94ff81a1f
Anthony140823 <anthonyreyes140803@gmail.com>
2026-05-01 16:57:56 -0500

    titulo de la funcion

--- agregar_producto.py
+++ agregar_producto.py
@@ -2,11 +2,12 @@
 
 # Función para agregar un nuevo producto:
 def agregar_producto(inventario, nombre:str, precio:float, cantidad:int):
+    print("--- Agregar Nuevo Producto ---")
     # calculamos el tamaño de la lista
     # para saber qué número de id le corresponde a cada producto
     tamaño =len(inventario)
     id = tamaño + 1
-    
+
     # Incializamos el diccionario que alvergará los datos del producto
     producto = {
         "id": id,
